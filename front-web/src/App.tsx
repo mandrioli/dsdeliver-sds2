b88075ae0a6b1d96fc9ee045af8cda9b61bdfb3a
Marcel Andrioli <marcelandrioli@gmail.com>
2021-01-07 19:04:26 -0300

    Add routes and StepHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
+import Routes from './Routes';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <Routes />
   );
 }
 
